498010bbd1bb3f9044f277c648f9aaabdb43fd91
feliks2810 <kepokali570@gmail.com>
2026-05-15 12:12:54 +0700

    chore: cleanup test scripts, add railway deployment config

--- create_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1;port=3309', 'root', '');
-    $pdo->exec('CREATE DATABASE IF NOT EXISTS portfolio_yfh');
-    echo 'Database created successfully';
-} catch (PDOException $e) {
-    echo $e->getMessage();
-}
